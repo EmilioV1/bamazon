1074d4e7cfc86572170ce3d7e499f1ddc3d56786
Emilio Valladares <vallae.wumbo@gmail.com>
2019-12-26 20:27:17 -0800

    Connection created, bamazon schema, and first from to customer created.

---
+++ shipment.sql
@@ -0,0 +1,13 @@
+INSERT INTO products(ProductName, DepartmentName, Price, StockQuantity)
+VALUES ("Uncharted 4", "Video Games", 49.95, 150),
+("DOOM", "Video Games", 59.99, 200),
+("Call of Duty: Modern Warfare", "Video Games", 54.99, 90),
+("Destiny 2", "Video Games", 49.95, 80),
+("Mario Kart", "Video Games", 49.95, 60),
+("Grand Theft Auto V", "Video Games", 59.99, 240),
+("Mortal Kombat", "Video Games", 49.95, 150),
+("Prince of Persia: Sands of Time", "Video Games", 49.95, 25),
+("Portal", "Video Games", 30.99, 150),
+("Tetris", "Video Games", 49.95, 150);
+
+SELECT * FROM bamazon.products;
